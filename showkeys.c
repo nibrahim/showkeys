fec09292470d134c94e837f2b49f79cfb6280c79
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-11-10 23:18:08 +0530

    Implemented proper formatting and parsing of Events

--- showkeys.c
+++ showkeys.c
@@ -1,7 +1,8 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <X11/Xlib.h> 
-
+#include <X11/Xutil.h>
 #include "showkeys.h"
 
 void 
@@ -13,25 +14,85 @@ usage()
 
 
 void
-parse_args(int nargs, char **args, Window *window_id) {
+parse_args(int nargs, char **args, Window *window_id) 
+{
   if (nargs != 2) {
     usage();
   }
   sscanf(args[1], "0x%lx", window_id);
 }
 
+int 
+process_modifiers(KeySym ks, int * meta, int *ctrl, int *shift, int val)
+{
+  int modifier_pressed = 0;
+  switch(ks) {
+    case XK_Shift_L:
+    case XK_Shift_R:
+      *shift = val;
+      modifier_pressed = 1;
+      break;
+    case XK_Control_L:
+    case XK_Control_R:
+      *ctrl = val;
+      modifier_pressed = 1;
+      break;
+    case XK_Alt_L:
+    case XK_Alt_R:
+      *meta = val;       /* This is not accurate but it's correct for my keyboard mappings */
+      modifier_pressed = 1;
+      break;
+  }
+  return modifier_pressed;
+}
+
+char * 
+create_emacs_keyname(char *keyname, int meta, int ctrl, int shift)
+{
+  char *retval;
+  asprintf(&retval, "%s%s%s%s", ctrl?"C-":"", meta?"M-":"", shift?"S-":"", keyname);
+  return retval;
+}
+
+
 void 
-poll_inputs(Display *display, Window window) {
+poll_inputs(Display *display, Window window) 
+{
   XEvent event;
-  XSelectInput(display, window, KeyPressMask);
+  XKeyEvent *e;
+  int meta = 0;
+  int ctrl = 0;
+  int shift = 0;
+  char str[256+1];
+  KeySym ks;
+  char *ksname;
+  char *display_string;
+  XSelectInput(display, window, KeyPressMask|KeyReleaseMask);
   while (1) {
     XNextEvent(display, &event);
-    printf("We're here\n");
+    e = (XKeyEvent *) &event;
+    XLookupString (e, str, 256, &ks, NULL);
+    ksname = XKeysymToString (ks); /* TBD: Might have to handle no symbol keys */
+    switch (event.type) {
+      case KeyPress:
+	if (! process_modifiers(ks, &meta, &ctrl, &shift, 1)) {
+	  display_string = create_emacs_keyname(ksname, meta, ctrl, shift);
+	  printf("%s", display_string);
+	  free(display_string);
+	}
+	break;
+      case KeyRelease:
+	process_modifiers(ks, &meta, &ctrl, &shift, 0);
+	break;
+    }
+    
+    
   }
 }
 
 int
-main(int argc, char **argv) {
+main(int argc, char **argv) 
+{
   Window window;
   Display *display;
 
@@ -43,6 +104,7 @@ main(int argc, char **argv) {
     exit (-1);
   }
 
+  XSetInputFocus(display, window, RevertToParent, CurrentTime);
   poll_inputs(display, window);
 
   XCloseDisplay(display);
--- showkeys.h
+++ showkeys.h
@@ -1,5 +1,7 @@
-#include <X11/Xlib.h> 
+#include <X11/Xlib.h>
 
 void parse_args(int, char **, Window *);
 void usage();
 void poll_inputs(Display *, Window);
+char *create_emacs_keyname(char *, int, int, int);
+int process_modifiers(KeySym , int * , int *, int *, int);
